28f6c70ba7455da6575eb6d402084578690bda8d
Simon Dobson <simon.dobson@computer.org>
2017-02-23 16:08:46 +0000

    Changed code license to GPLv3

--- test/sirsynchronous.py
+++ test/sirsynchronous.py
@@ -1,10 +1,21 @@
-# Tests of SIR under stochastic dynamics
+# Tests of SIR under synchronous dynamics
 #
-# Copyright (C) 2014-2016 Simon Dobson
+# Copyright (C) 2014-2017 Simon Dobson
 # 
-# Licensed under the Creative Commons Attribution-Noncommercial-Share
-# Alike 3.0 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+# This file is part of Complex networks, complex processes (CNCP).
 #
+# CNCP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CNCP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CNCP. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 from cncp import *
 from .sir import SIRTests
