71a30b1d7421c27796d663a11bc004c7ecb414d0
Simon Dobson <simon.dobson@computer.org>
2021-02-10 16:20:11 +0000

    Switched to use generators and a process sequence

--- utils/make-monitor-progress.py
+++ utils/make-monitor-progress.py
@@ -66,18 +66,18 @@ for t in range(0, T):
 
 fig = plt.figure(figsize = (5, 5))
 ax = fig.gca()
-plt.title('Progress of an infection $(\\alpha = {a}, \\beta = {b})$'.format(a = pRecover, b = pInfect))
+plt.title('Progress of an infection $(\\alpha = {a}, \\beta = {b})$'.format(a=pRecover, b=pInfect))
 plt.xlabel('time')
 ax.set_xlim([0, T])
 plt.ylabel('fraction of population that is...')
 ax.set_ylim([0.0, 1.0])
-plt.plot(sss, 'y', label = 'susceptible')
-plt.plot(iis, 'r', label = 'infected')
-plt.plot(rrs, 'g', label = 'removed')
+plt.plot(sss, 'y', label='susceptible')
+plt.plot(iis, 'r', label='infected')
+plt.plot(rrs, 'g', label='removed')
 plt.legend(loc = 'upper right')
 plt.savefig('doc/cookbook/sir-progress-dt.png')
 
-class MonitoredSIR(epydemic.SIR, epydemic.Monitor):
+class MonitoredSIR(epydemic.SIR):
 
     def __init__(self):
         super(MonitoredSIR, self).__init__()
@@ -93,18 +93,17 @@ class MonitoredSIR(epydemic.SIR, epydemic.Monitor):
         self.trackNodesInCompartment(epydemic.SIR.REMOVED)
 
 N = 10000
-
 kmean = 3
-phi = kmean / N
-er_g = networkx.erdos_renyi_graph(N, phi)
 
 params = dict()
+params[epydemic.ERNetwork.N] = N
+params[epydemic.ERNetwork.KMEAN] = kmean
 params[epydemic.SIR.P_INFECT] = pInfect
 params[epydemic.SIR.P_REMOVE] = pRecover
 params[epydemic.SIR.P_INFECTED] = pInfected
 params[epydemic.Monitor.DELTA] = 10
 
-e = epydemic.StochasticDynamics(MonitoredSIR(), g = er_g)
+e = epydemic.StochasticDynamics(epydemic.ProcessSequence([MonitoredSIR(), epydemic.Monitor()]), epydemic.ERNetwork())
 e.process().setMaximumTime(T)
 rc = e.set(params).run()
 
@@ -117,70 +116,23 @@ plt.ylabel('fraction of population that is...')
 ax.set_ylim([0.0, 1.0])
 timeseries = rc[epyc.Experiment.RESULTS][epydemic.Monitor.TIMESERIES]
 ts = timeseries[epydemic.Monitor.OBSERVATIONS]
-er_sss = list(map(lambda v: v / er_g.order(), timeseries[epydemic.SIR.SUSCEPTIBLE]))
-er_iis = list(map(lambda v: v / er_g.order(), timeseries[epydemic.SIR.INFECTED]))
-er_rrs = list(map(lambda v: v / er_g.order(), timeseries[epydemic.SIR.REMOVED]))
-plt.plot(ts, er_sss, 'y', label = 'susceptible')
-plt.plot(ts, er_iis, 'r', label = 'infected')
-plt.plot(ts, er_rrs, 'g', label = 'removed')
+er_sss = list(map(lambda v: v / N, timeseries[epydemic.SIR.SUSCEPTIBLE]))
+er_iis = list(map(lambda v: v / N, timeseries[epydemic.SIR.INFECTED]))
+er_rrs = list(map(lambda v: v / N, timeseries[epydemic.SIR.REMOVED]))
+plt.plot(ts, er_sss, 'y', label='susceptible')
+plt.plot(ts, er_iis, 'r', label='infected')
+plt.plot(ts, er_rrs, 'g', label='removed')
 plt.legend(loc = 'upper right')
 plt.savefig('doc/cookbook/sir-progress-er.png')
 
-def makePowerlawWithCutoff(alpha, kappa):
-    '''Create a model function for a powerlaw distribution with exponential cutoff.
-
-    :param alpha: the exponent of the distribution
-    :param kappa: the degree cutoff
-    :returns: a model function'''
-    C = Li(alpha, math.exp(-1.0 / kappa))
-    def p( k ):
-        return (pow((k + 0.0), -alpha) * math.exp(-(k + 0.0) / kappa)) / C
-    return p
-
-def generateFrom(N, p, maxdeg=100):
-    # construct degrees according to the distribution given
-    # by the model function
-    ns = []
-    t = 0
-    for i in range(N):
-        while True:
-            k = rng.integers(1, maxdeg)
-            if rng.random() < p(k):
-                ns.append(k)
-                t += k
-                break
-
-    # the final sequence of degrees has to sum to an even number, as
-    # each edge has two endpoints
-    # if the sequence is odd, remove an element and draw another from
-    # the distribution, repeating until the overall sequence is even
-    while t % 2 != 0:
-        # pick a node at random
-        i = rng.integers(0, len(ns) - 1)
-
-        # remove it from the sequence and from the total
-        t -= ns[i]
-        del ns[i]
-            
-        # draw a new degree from the distribution
-        while True:
-            k = rng.integers(1, maxdeg)
-            if rng.random() < p(k):
-                # add new node to the sequence
-                ns.append(k)
-                t += k
-                break
-
-    # populate the network using the configuration
-    # model with the given degree distribution
-    g = networkx.configuration_model(ns, create_using=networkx.Graph())
-    return g
-
 kappa = 10
 alpha = 2
-plc_g = generateFrom(N, makePowerlawWithCutoff(alpha, kappa))
 
-e = epydemic.StochasticDynamics(MonitoredSIR(), g = plc_g)
+params[epydemic.PLCNetwork.N] = N
+params[epydemic.PLCNetwork.EXPONENT] = alpha
+params[epydemic.PLCNetwork.CUTOFF] = kappa
+
+e = epydemic.StochasticDynamics(epydemic.ProcessSequence([MonitoredSIR(), epydemic.Monitor()]), epydemic.PLCNetwork())
 e.process().setMaximumTime(T)
 rc = e.set(params).run()
 
@@ -193,11 +145,11 @@ plt.ylabel('fraction of population that is...')
 ax.set_ylim([0.0, 1.0])
 timeseries = rc[epyc.Experiment.RESULTS][epydemic.Monitor.TIMESERIES]
 ts = timeseries[epydemic.Monitor.OBSERVATIONS]
-plc_sss = list(map(lambda v: v / er_g.order(), timeseries[epydemic.SIR.SUSCEPTIBLE]))
-plc_iis = list(map(lambda v: v / er_g.order(), timeseries[epydemic.SIR.INFECTED]))
-plc_rrs = list(map(lambda v: v / er_g.order(), timeseries[epydemic.SIR.REMOVED]))
-plt.plot(ts, plc_sss, 'y', label = 'susceptible')
-plt.plot(ts, plc_iis, 'r', label = 'infected')
-plt.plot(ts, plc_rrs, 'g', label = 'removed')
+plc_sss = list(map(lambda v: v / N, timeseries[epydemic.SIR.SUSCEPTIBLE]))
+plc_iis = list(map(lambda v: v / N, timeseries[epydemic.SIR.INFECTED]))
+plc_rrs = list(map(lambda v: v / N, timeseries[epydemic.SIR.REMOVED]))
+plt.plot(ts, plc_sss, 'y', label='susceptible')
+plt.plot(ts, plc_iis, 'r', label='infected')
+plt.plot(ts, plc_rrs, 'g', label='removed')
 plt.legend(loc = 'upper right')
 plt.savefig('doc/cookbook/sir-progress-plc.png')
