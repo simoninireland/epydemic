c922ec5904b547b7630a041e9eb60075ae572ca0
Simon Dobson <simon.dobson@computer.org>
2021-04-26 09:45:06 +0100

    Restored deleted tests

--- test/test_bbt.py
+++ test/test_bbt.py
@@ -23,7 +23,7 @@ import unittest
 
 
 class BBTHelper(object):
-    '''Set wrapper to make testign easier.'''
+    '''Set wrapper to make testing easier.'''
 
     def __init__(self):
         self._root = None
@@ -102,46 +102,268 @@ class BBTHelper(object):
 
 class BBTTest(unittest.TestCase):
 
+    def assertInvariant(self, s):
+        '''Test the invariant of tree is satisfied.'''
+        if s._root is not None:
+            self.assertAVLInvariant(s._root)
+
+    def assertAVLInvariant(self, n):
+        lh = n._left._height + 1 if n._left is not None else 0
+        rh = n._right._height + 1 if n._right is not None else 0
+        self.assertTrue(abs(lh - rh) <= 1)
+        self.assertEqual(n._height, max(lh, rh))
+        if lh > 0:
+            self.assertIsNotNone(n._left)
+            self.assertAVLInvariant(n._left)
+        if rh > 0:
+            self.assertIsNotNone(n._right)
+            self.assertAVLInvariant(n._right)
+
     def testEmptySet(self):
         '''Test empty sets.'''
         s = BBTHelper()
+        self.assertInvariant(s)
         self.assertEqual(len(s), 0)
         self.assertFalse(1 in s)
         self.assertCountEqual(s, [])
+        self.assertTrue(s.empty())
 
     def testAdd(self):
         '''Test we can add individual elements.'''
         s = BBTHelper()
+        self.assertTrue(s.empty())
         s.add(1)
+        self.assertFalse(s.empty())
+        self.assertInvariant(s)
         self.assertEqual(len(s), 1)
         self.assertTrue(1 in s)
         s.add(2)
+        self.assertInvariant(s)
         self.assertEqual(len(s), 2)
         self.assertTrue(1 in s)
         self.assertTrue(2 in s)
-        self.assertCountEqual(s.elements(), [1,2])
-        self.assertCountEqual(s, [1, 2])
+        self.assertCountEqual(list(s), [1,2])
+        s.add(3)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 3)
+        self.assertTrue(1 in s)
+        self.assertTrue(2 in s)
+        self.assertTrue(3 in s)
+        self.assertCountEqual(list(s), [1, 2, 3])
+
+    def testABCroot(self):
+        '''Test the ABC rotation over the root.'''
+        s = BBTHelper()
+        s.add(0)
+        s.add(1)
+        s.add(2)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [0, 1, 2])
+        self.assertEqual(s._root._data, 1)
+
+    def testABC(self):
+        '''Test the ABC rotation below the root.'''
+        s = BBTHelper()
+        s.add(1)
+        s.add(0)
+        s.add(2)
+        s.add(3)
+        s.add(4)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [0, 1, 2, 3, 4])
+        self.assertEqual(s._root._data, 1)
+
+    def testACBroot(self):
+        '''Test the ACB rotation overt the root.'''
+        s = BBTHelper()
+        s.add(1)
+        s.add(3)
+        s.add(2)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [1, 2, 3])
+        self.assertEqual(s._root._data, 2)
+
+    def testACB(self):
+        '''Test the ACB rotation below the root.'''
+        s = BBTHelper()
+        s.add(3)
+        s.add(0)
+        s.add(5)
+        s.add(7)
+        s.add(6)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [0, 3, 5, 6, 7])
+        self.assertEqual(s._root._data, 3)
+
+    def testCBAroot(self):
+        '''Test the CBA rotation across the root.'''
+        s = BBTHelper()
+        s.add(3)
+        s.add(2)
+        s.add(1)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [1, 2, 3])
+        self.assertEqual(s._root._data, 2)
+
+    def testCBA(self):
+        '''Test the CBA rotation below the root.'''
+        s = BBTHelper()
+        s.add(5)
+        s.add(7)
+        s.add(3)
+        s.add(2)
+        s.add(1)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [1, 2, 3, 5, 7])
+        self.assertEqual(s._root._data, 5)
+
+    def testCABroot(self):
+        '''Test the CABB rotation across the root.'''
+        s = BBTHelper()
+        s.add(4)
+        s.add(2)
+        s.add(3)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [2, 3, 4])
+        self.assertEqual(s._root._data, 3)
+
+    def testCAB(self):
+        '''Test the CAB rotation below the root.'''
+        s = BBTHelper()
+        s.add(2)
+        s.add(1)
+        s.add(8)
+        s.add(5)
+        s.add(6)
+        self.assertInvariant(s)
+        self.assertCountEqual(list(s), [1, 2, 5, 6, 8])
+        self.assertEqual(s._root._data, 2)
 
     def testDiscard(self):
         '''Test we can discard elements that are present.'''
         s = BBTHelper()
-        s.add(1)
         s.add(2)
-        s.discard(1)
+        s.add(3)
+        s.discard(3)
+        self.assertInvariant(s)
         self.assertEqual(len(s), 1)
         self.assertCountEqual(s, [2])
 
-    def testRepeateAdd(self):
-        '''Test we can add the same element twice.'''
+    def testDiscardEmpty(self):
+        '''Test we can discard from an empty set.'''
+        s = BBTHelper()
+        s.discard(1)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 0)
+        self.assertCountEqual(s, [])
+
+    def testRemoveSuccess(self):
+        '''Test successful removal.'''
         s = BBTHelper()
         s.add(1)
         s.add(2)
+        s.remove(2)
+        self.assertInvariant(s)
+
+    def testRemoveFailure(self):
+        '''Test failing removal of a non-element.'''
+        s = BBTHelper()
+        s.add(1)
+        s.add(2)
+        with self.assertRaises(KeyError):
+            s.remove(3)
+
+    def testRemoveEmpty(self):
+        '''Test removal; from an empty set.'''
+        s = BBTHelper()
+        with self.assertRaises(KeyError):
+            s.remove(3)
+
+    def testDiscardToEmpty(self):
+        '''Test we can discard the last element in the set.'''
+        s = BBTHelper()
+        s.add(1)
+        s.discard(1)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 0)
+        self.assertCountEqual(s, [])
+        self.assertTrue(s.empty())
+
+    def testRemoveToEmpty(self):
+        '''Test we can removethe last element in the set.'''
+        s = BBTHelper()
+        s.add(1)
+        s.remove(1)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 0)
+        self.assertCountEqual(s, [])
+        self.assertTrue(s.empty())
+
+    def testDiscardLeaves(self):
+        '''Test we can discard leaf elements.'''
+        s = BBTHelper()
+        s.add(2)
+        s.add(3)
+        s.discard(3)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 1)
+        self.assertCountEqual(s, [2])
+        s.add(1)
+        s.discard(1)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 1)
+        self.assertCountEqual(s, [2])
+        s.discard(2)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 0)
+        self.assertCountEqual(s, [])
+
+    def testDiscardSingleSubtree(self):
+        '''Test we can discard nodes with a single sub-tree.'''
+        s = BBTHelper()
+        s.add(2)
+        s.add(3)
+        s.add(4)
+        s.add(5)
+        s.discard(4)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 3)
+        self.assertCountEqual(s, [2, 3, 5])
+        s.add(1)
+        s.discard(2)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 3)
+        self.assertCountEqual(s, [1, 3, 5])
+
+    def testDiscardTwoSubtrees(self):
+        '''Test we can discard a node with two sub-trees.'''
+        s = BBTHelper()
+        s.add(3)
+        s.add(2)
+        s.add(10)
+        s.add(7)
+        s.add(15)
+        s.add(6)
+        s.discard(10)
+        self.assertInvariant(s)
+        self.assertEqual(len(s), 5)
+        self.assertCountEqual(s, [2, 3, 6, 7, 15])
+
+    def testRepeatedAdd(self):
+        '''Test we can add the same element twice.'''
+        s = BBTHelper()
+        s.add(1)
+        self.assertInvariant(s)
+        s.add(2)
+        self.assertInvariant(s)
         self.assertEqual(len(s), 2)
         self.assertCountEqual(s, [1, 2])
         s.add(2)
+        self.assertInvariant(s)
         self.assertEqual(len(s), 2)
         self.assertCountEqual(s, [1, 2])
         s.add(1)
+        self.assertInvariant(s)
         self.assertEqual(len(s), 2)
         self.assertCountEqual(s, [1, 2])
 
@@ -163,7 +385,7 @@ class BBTTest(unittest.TestCase):
         self.assertCountEqual(s, [])
 
     def testRepeatedDiscard(self):
-        '''Test repeated doiscarding.'''
+        '''Test repeated discarding.'''
         s = BBTHelper()
         s.add(1)
         s.add(2)
@@ -176,51 +398,71 @@ class BBTTest(unittest.TestCase):
         self.assertEqual(len(s), 1)
         self.assertCountEqual(s, [2])
 
+    def testRepeatedRemove(self):
+        '''Test repeated removing.'''
+        s = BBTHelper()
+        s.add(1)
+        s.add(2)
+        self.assertEqual(len(s), 2)
+        self.assertCountEqual(s, [1, 2])
+        s.remove(1)
+        self.assertEqual(len(s), 1)
+        self.assertCountEqual(s, [2])
+        with self.assertRaises(KeyError):
+            s.remove(1)
+
     def testLotsOfAdds(self):
         '''Test we can add loads of elements, and get the results in order.'''
         s = BBTHelper()
-        es = list(range(1000))
+        es = list(range(200))
         rs = es.copy()
         numpy.random.shuffle(es)
-        for i in rs:
+        for i in es:
             s.add(i)
+            self.assertInvariant(s)
         self.assertEqual(len(s), len(rs))
         self.assertCountEqual(rs, s.elements())
 
-    def testAddDeleteInOrder(self):
+    def testAddRemoveInOrder(self):
         '''Test we can handle an maximally inbalanced tree.'''
         s = BBTHelper()
-        es = list(range(1000))
+        es = list(range(20))
         for i in es:
             s.add(i)
+            self.assertInvariant(s)
         self.assertEqual(len(s), len(es))
         for i in es:
-            s.discard(i)
+            s.remove(i)
+            self.assertInvariant(s)
         self.assertEqual(len(s), 0)
 
-    def testAddDeleteInReverseOrder(self):
+    def testAddRemoveInReverseOrder(self):
         '''Test we can handle an maximally inbalanced tree the other way.'''
         s = BBTHelper()
-        es = list(range(1000))
+        es = list(range(100))
         es.reverse()
         for i in es:
             s.add(i)
+            self.assertInvariant(s)
         self.assertEqual(len(s), len(es))
         for i in es:
-            s.discard(i)
+            s.remove(i)
+            self.assertInvariant(s)
         self.assertEqual(len(s), 0)
 
-    def testAddDelete(self):
-        '''Test we can add in random order and then delete in order.'''
+    def testAddRemove(self):
+        '''Test we can add in random order and then remove in order.'''
         s = BBTHelper()
         es = list(range(100))
         rs = es.copy()
-        numpy.random.shuffle(es)
+        numpy.random.shuffle(rs)
         for i in rs:
             s.add(i)
+            self.assertInvariant(s)
         l = len(s)
         for j in es:
-            s.discard(j)
+            s.remove(j)
+            self.assertInvariant(s)
             l -= 1
             self.assertEqual(len(s), l)
         self.assertEqual(len(s), 0)
@@ -228,8 +470,29 @@ class BBTTest(unittest.TestCase):
     def testDraw(self):
         '''Test the basic draw functionality.'''
         s = BBTHelper()
-        es = list(range(100))
-        for i in es:
-            s.add(i)
-        j = s.draw()
-        self.assertIn(j, s)
+        es = list(range(1000))
+        numpy.random.shuffle(es)
+        for e in es:
+            s.add(e)
+        for _ in range(int(len(es) * 0.1)):
+            j = s.draw()
+            self.assertIn(j, s)
+
+    def testDrawAll(self):
+        '''Test we always draw all elements.'''
+        s = BBTHelper()
+        es = list(range(1000))
+        numpy.random.shuffle(es)
+        for e in es:
+            s.add(e)
+        ns = []
+        for _ in range(len(es)):
+            n = s.draw()
+            self.assertIsNotNone(n)
+            self.assertNotIn(n, ns)
+            ns.append(n)
+            s.remove(n)
+            self.assertInvariant(s)
+            self.assertNotIn(n, s)
+        self.assertEqual(len(s), 0)
+        self.assertCountEqual(es, ns)
--- test/test_loci.py
+++ test/test_loci.py
@@ -1,7 +1,7 @@
 # Test basic loci functions
 #
 # Copyright (C) 2017--2020 Simon Dobson
-# 
+#
 # This file is part of epydemic, epidemic network simulations in Python.
 #
 # epydemic is free software: you can redistribute it and/or modify
@@ -22,17 +22,48 @@ import unittest
 
 class LociTest(unittest.TestCase):
 
+    # the correctness tests for the underlying tree
+    # appear in test/test_bbt.py
+
     def testName(self):
         '''Test name retrieval.'''
         l = Locus('test')
         self.assertEqual(l.name(), 'test')
+
+    def testAdd(self):
+        '''Test we can add elements.'''
+        l = Locus('test')
+        self.assertEqual(len(l), 0)
+        l.add(1)
+        self.assertEqual(len(l), 1)
+        l.add(2)
+        self.assertEqual(len(l), 2)
+
+    def testDelete(self):
+        '''Test we can remove elements.'''
+        l = Locus('test')
+        l.add(1)
+        l.add(2)
+        l.discard(1)
+        self.assertEqual(len(l), 1)
+        l.discard(2)
+        self.assertEqual(len(l), 0)
 
     def testDrawEmpty(self):
         '''Test we can't draw from an empty locus.'''
         l = Locus('test')
         with self.assertRaises(ValueError):
             l.draw()
- 
+
+    def testDraw(self):
+        '''Test the basic draw functionality.'''
+        l = Locus('test')
+        es = list(range(100))
+        for i in es:
+            l.add(i)
+        j = l.draw()
+        self.assertIn(j, l)
+
     def testDrawOne(self):
         '''Test we can draw from an empty locus.'''
         l = Locus('test')
@@ -50,6 +81,7 @@ class LociTest(unittest.TestCase):
         e = l.draw()
         self.assertIn(e, [ 1, 2 ])
         self.assertCountEqual(l.elements(), [ 1, 2 ])
+
 
 if __name__ == '__main__':
     unittest.main()
