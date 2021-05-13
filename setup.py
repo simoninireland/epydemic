b855521c93f0d40a0b66cc54755e8a05d1196c6e
Simon Dobson <simon.dobson@computer.org>
2021-05-13 13:21:29 +0100

    Ready for release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.rst') as f:
     longDescription = f.read()
 
 setup(name = 'epydemic',
-      version = '1.5.1',
+      version = '1.6.1',
       description = 'Epidemic network simulations in Python',
       long_description = longDescription,
       url = 'http://github.com/simoninireland/epydemic',
