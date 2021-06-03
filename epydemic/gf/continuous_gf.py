5247f4e4c84075417feaad08a24261ac1297ecdc
Simon Dobson <simon.dobson@computer.org>
2021-06-03 17:29:05 +0100

    Changed to mpmath.factorial to allow higher degrees

--- epydemic/gf/continuous_gf.py
+++ epydemic/gf/continuous_gf.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-import math
+from mpmath import factorial
 from typing import Callable
 from epydemic.gf import GF
 import numpy
@@ -40,12 +40,15 @@ class ContinuousGF(GF):
 
     '''
 
-    def __init__(self, f: Callable[float, float]):
+    def __init__(self, f: Callable[[float], float]):
         super().__init__()
-
         self._f = f
 
-    def _differentiate(self, z: int, n: int = 1, r: float = 1.0, dx: float = 1e-2):
+        # make sure the function vectorises
+        if not isinstance(self._f, numpy.vectorize):
+            self._f = numpy.vectorize(self._f)
+
+    def _differentiate(self, z: int, n: int = 1, r: float = 1.0, dx: float = 1e-2) -> complex:
         '''Compute the n'th derivative of f at z using
         a Cauchy contour integral of radius r.
 
@@ -53,24 +56,19 @@ class ContinuousGF(GF):
         :param n: order of derivative (defaults to 1)
         :param r: radius of contour (defaults to 1.0)
         :param dx: step size (defaults to 1e-2)'''
-
-        # make sure the function vectorises
-        if not isinstance(self._f, numpy.vectorize):
-            self._f = numpy.vectorize(self._f)
-
         x = r * numpy.exp(2j * numpy.pi * numpy.arange(0, 1, dx))
-        return math.factorial(n) * numpy.mean(self._f(z + x) / x**n)
+        return factorial(n) * numpy.mean(self._f(z + x) / x**n)
 
     def getCoefficient(self, i: int) -> float:
         '''Return the i'th coefficient.
 
         :param i: the index
         :returns: the coefficient of x^i'''
-        return self._differentiate(0.0, i)
+        return float(self._differentiate(0.0, i).real / factorial(i))
 
     def evaluate(self, x: float) -> float:
         '''Evaluate the generating function.
 
         :param x: the argument
         :returns: the value of the generating function'''
-        return self._f(x)
+        return self._f(x).real
