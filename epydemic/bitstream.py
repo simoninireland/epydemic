a776b0e1473239b9dcf521844e8b9da76642e5f9
Simon Dobson <simon.dobson@computer.org>
2022-07-25 13:25:47 +0100

    Restored deleted numpy import

--- epydemic/bitstream.py
+++ epydemic/bitstream.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
+import numpy
 from epydemic import rng
 from typing import Optional, List, Iterable
 
